dbb9195d6968efbaa76e02250229da4a11e67c0c
AmrHishamFarouk <hishamamr121@gmail.com>
2024-03-23 23:38:44 +0200

    Add files via upload

---
+++ tictactoe.js
@@ -0,0 +1,30 @@
+function Gameboard (){
+    let gameboard = [];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// function createplayer (player,symbol){
+//     let name = player;
+//     let score = 0;
+//     let givescore = () => score++;
+//     let getscore = () => score;
+//     return {name,symbol,givescore,getscore};
+// }
+
+
+
+
+
+// const amr = createplayer('amr','x');
+// amr.givescore();
+//  console.log(amr);
